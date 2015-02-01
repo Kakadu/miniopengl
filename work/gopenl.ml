2f77113c7cf8fd2db7e171f61446491f25350e1c
kakadu <kakadu.hafanana@gmail.com>
2015-02-01 15:06:23 +0300

    Drawing African Head (1st article finished)

--- work/gopenl.ml
+++ work/gopenl.ml
@@ -19,3 +19,34 @@ module Image = struct
 
 
 end
+
+let rec line ~color ~img x0 y0 x1 y1 =
+  let rec helper steep x0 y0 x1 y1 =
+    (*printf "Drawing %b %d %d %d %d\n%!" steep x0 y0 x1 y1;*)
+    let dx = x1-x0 in
+    let dy = y1-y0 in
+
+    let derror2 = 2 * abs dy in
+    let error2  = ref 0 in
+    let y = ref y0 in
+
+    for x=x0 to x1 do
+      if steep
+      then Image.set ~img ~color !y  x
+      else Image.set ~img ~color  x !y;
+
+      error2 := !error2 + derror2;
+      if !error2 > dx then ( if y1>y0 then incr y else decr y; error2 := !error2 - 2*dx );
+    done;
+    ()
+  in
+
+  let (steep, x0, y0, x1, y1) =
+    if abs (x0 - x1) < abs (y0 - y1)
+    then (true, y0, x0, y1, x1)
+    else (false, x0, y0, x1, y1)
+  in
+  let (steep, x0, y0, x1, y1) =
+    if x0>x1 then (steep, x1, y1, x0, y0) else (steep, x0, y0, x1, y1)
+  in
+  helper steep x0 y0 x1 y1
--- work/main.ml
+++ work/main.ml
@@ -11,217 +11,84 @@ let rec _dline2 () = if false then _dline2 ()
 let rec _dline3 () = if false then _dline3 ()
 let rec _dline4 () = if false then _dline4 ()
 
-
-
 open Gopenl
 
-let rec line0 ~color ~img x0 y0 x1 y1 =
-  _dline0 ();
+type image_data =
+  { vertexes: (float*float*float) array
+  ; triangles: (int*int*int) list
+  }
 
-  let rec helper steep x0 y0 x1 y1 =
-    for x= x0 to x1 do
-      let t = float_of_int (x - x0) /. float_of_int (x1 - x0) in
-      let y = float_of_int y0 *. (1. -. t) +. (float_of_int y1  *. t) in
-      if steep
-      then Image.set ~img ~color (int_of_float y) x
-      else Image.set ~img ~color x (int_of_float y)
-    done;
-    ()
+let get_vertex ~n (data: image_data) =
+  let len = Array.length data.vertexes in
+  if n >= len then failwith (sprintf "Can't get vertex %d in array len=%d" n len);
+  data.vertexes.(n)
+
+let read_image_data filename =
+  let ch = open_in filename in
+  let rec loop verts tris =
+    try
+      let str = input_line ch in
+      if str = "" then loop verts tris
+      else if Str.first_chars str 2 = "vt" then loop verts tris
+      else if Str.first_chars str 2 = "vn" then loop verts tris
+      else if Str.first_chars str 1 = "#"  then loop verts tris
+      else if Str.first_chars str 1 = "g"  then loop verts tris
+      else if Str.first_chars str 1 = "s"  then loop verts tris
+      else if Str.first_chars str 2 = "f " then (
+        let tri = Scanf.sscanf str "f %d/%d/%d %d/%d/%d %d/%d/%d" (fun n1 _ _ n2 _ _ n3 _ _ -> (n1,n2,n3) ) in
+        loop verts (tri::tris)
+      )
+      else if Str.first_chars str 2 = "v " then (
+        let v = Scanf.sscanf str "v %f %f %f" (fun x y z -> (x,y,z) ) in
+        loop (v::verts) tris
+      ) else failwith "Reading file is not fully implemented"
+    with End_of_file -> (verts, tris)
   in
-
-  let (steep, x0, y0, x1, y1) =
-    if abs (x0 - x1) < abs (y0 - y1)
-    then (true, y0, x0, y1, x1)
-    else (false, x0, y0, x1, y1)
-  in
-  let (steep, x0, y0, x1, y1) =
-    if x0>x1 then (steep, x1, y1, x0, y0) else (steep, x0, y0, x1, y1)
-  in
-  helper steep x0 y0 x1 y1;
-  _dline1 ()
-
-let rec line4 ~color ~img x0 y0 x1 y1 =
-  _dline0 ();
-
-  let rec helper steep x0 y0 x1 y1 =
-    let dx = x1-x0 in
-    let dy = y1-y0 in
-
-    let derror2 = 2 * abs dy in
-    let error2  = ref 0 in
-    let y = ref y0 in
-
-    for x=x0 to x1 do
-      if steep
-      then Image.set ~img ~color !y  x
-      else Image.set ~img ~color  x !y;
-
-      error2 := !error2 + derror2;
-      if !error2 > dx then ( if y1>y0 then incr y else decr y; error2 := !error2 - 2*dx );
-    done;
-    ()
-  in
-
-  let (steep, x0, y0, x1, y1) =
-    if abs (x0 - x1) < abs (y0 - y1)
-    then (true, y0, x0, y1, x1)
-    else (false, x0, y0, x1, y1)
-  in
-  let (steep, x0, y0, x1, y1) =
-    if x0>x1 then (steep, x1, y1, x0, y0) else (steep, x0, y0, x1, y1)
-  in
-  helper steep x0 y0 x1 y1;
-  _dline1 ()
-
-let rec line ~color ~img x0 y0 x1 y1 =
-  _dline0 ();
-  let x0 = ref x0 in
-  let x1 = ref x1 in
-  let y0 = ref y0 in
-  let y1 = ref y1 in
-  let steep = ref false in
-
-  if abs (!x0 - !x1) < abs(!y0 - !y1) then begin
-      swap x0 y0;
-      swap x1 y1;
-      steep := true;
-  end;
-  if x0>x1 then begin
-      swap x0 x1;
-      swap y0 y1;
-  end;
-
-  for x= !x0 to !x1 do
-    let t = float_of_int (x - !x0) /. float_of_int (!x1 - !x0) in
-    let y = float_of_int !y0 *. (1. -. t) +. (float_of_int !y1  *. t) in
-    if !steep
-    then Image.set ~img ~color (int_of_float y) x
-    else Image.set ~img ~color x (int_of_float y)
-  done;
-  _dline1 ()
-
-let rec line2 ~color ~img x0 y0 x1 y1 =
-  _dline2 ();
-  let dx = x1-x0 in
-  let dy = y1-y0 in
-
-  let x0 = ref x0 in
-  let x1 = ref x1 in
-  let y0 = ref y0 in
-  let y1 = ref y1 in
-  let steep = ref false in
-
-  if abs (!x0 - !x1) < abs(!y0 - !y1) then begin
-      swap x0 y0;
-      swap x1 y1;
-      steep := true;
-  end;
-  if x0>x1 then begin
-      swap x0 x1;
-      swap y0 y1;
-  end;
-
-  let derror = (float_of_int @@ abs dy) /. (float_of_int @@ abs dx) in
-  let error  = ref 0. in
-  let y = ref !y0 in
-
-  for x= !x0 to !x1 do
-    if !steep
-    then Image.set ~img ~color !y  x
-    else Image.set ~img ~color  x !y;
-
-    error := !error +. derror;
-    if !error > 0.5 then ( (if y1>y0 then incr else decr) y; error := !error -. 1. );
-  done;
-  _dline3 ()
-
-let rec line3 ~color ~img x0 y0 x1 y1 =
-  _dline2 ();
-  let dx = x1-x0 in
-  let dy = y1-y0 in
-
-  let x0 = ref x0 in
-  let x1 = ref x1 in
-  let y0 = ref y0 in
-  let y1 = ref y1 in
-  let steep = ref false in
-
-  if abs (!x0 - !x1) < abs(!y0 - !y1) then begin
-      swap x0 y0;
-      swap x1 y1;
-      steep := true;
-  end;
-  if x0>x1 then begin
-      swap x0 x1;
-      swap y0 y1;
-  end;
-
-  let derror2 = 2 * abs dy in
-  let error2  = ref 0 in
-  let y = ref !y0 in
-
-  for x= !x0 to !x1 do
-    if !steep
-    then Image.set ~img ~color !y  x
-    else Image.set ~img ~color  x !y;
-
-    error2 := !error2 + derror2;
-    if !error2 > dx then ( (if y1>y0 then incr else decr) y; error2 := !error2 - 2*dx );
-  done;
-  _dline3 ()
+  let (verts, triangles) = loop [] [] in
+  close_in ch;
+  { vertexes = Array.of_list @@ List.rev verts; triangles }
 
 type config =
   { mutable line: color:Color.t -> img:Image.t -> int -> int -> int -> int -> unit
+  ; mutable doread: bool
   }
 
-let config = { line = line }
+let config = { line = Gopenl.line; doread=false }
 
 let main =
-  let img   = Image.create 100 100 in
   let red   = Color.make 255 0 0 in
   let green = Color.make 0 255 0 in
   let blue  = Color.make 0 0 255 in
   let yel   = Color.make 255 255 0 in
   let _ = [red; green; blue; yel] in
   let args =
-    [ ("-mode0", Arg.Unit (fun () -> config.line <- line0), "line0")
-    ; ("-mode1", Arg.Unit (fun () -> config.line <- line ), "line1")
-    ; ("-mode2", Arg.Unit (fun () -> config.line <- line2), "line2")
-    ; ("-mode3", Arg.Unit (fun () -> config.line <- line3), "line3")
-    ; ("-mode4", Arg.Unit (fun () -> config.line <- line4), "line4")
+    [ ("-read", Arg.Unit (fun () -> config.doread <- true), "really read")
     ]
   in
   Arg.parse args (fun s -> failwith @@ sprintf "No anonymous args '%s'\n%!" s) "usage";
-(*
-  Image.line ~img ~color:red 50 50 3  3;
-  Image.line ~img ~color:red 50 50 96 3;
-  Image.line ~img ~color:red 50 50 3  96;
-  Image.line ~img ~color:red 50 50 96 96;
-  *)
-  (*
-  Image.line ~img ~color:green 0 0 49 99;
-   *)
-(*
-  Image.line ~img ~color:green 25  0 50 99;
-  Image.line ~img ~color:red   50 0 75 99; *)
-(*
-  Image.line ~img ~color:blue   0 99 30 60;
-  Image.line ~img ~color:yel   99  0 70 99;
-  Image.line ~img ~color:red   99 77 13 13;
- *)
-(*
-  Image.line ~img ~color:red   49  0 49 99;
-  Image.line ~img ~color:blue   0  0 99  0;
-  Image.line ~img ~color:yel    0 10 99 40; *)
-(*
-  Image.line ~img ~color:green 50 50 3  25;
-  Image.line ~img ~color:green 50 50 25 96;
-  Image.line ~img ~color:green 50 50 96 25;
- *)
 
-  for n =1 to 1000000 do
-    config.line ~img ~color:blue   12 20 80 40;
-    config.line ~img ~color:blue   20 13 40 80;
-    config.line ~img ~color:blue   80 40 13 20;
-  done;
+  let width = 800 in
+  let height = 800 in
+  let img   = Image.create width height in
+
+  if config.doread then begin
+      let data = read_image_data "data/african_head.obj" in
+      printf "File is read: %d %d\n%!" (Array.length data.vertexes) (List.length data.triangles);
+      let f (n1,n2,n3) =
+        (* float argument is in range [ -1 .. +1 ] which should be project onto [0..(width-1)] *)
+        (* Also we mirror y *)
+        let p1 = get_vertex ~n:(n1-1) data in
+        let p2 = get_vertex ~n:(n2-1) data in
+        let p3 = get_vertex ~n:(n3-1) data in
+        let f = fun ( (x0f,y0f,_), (x1f,y1f,_) ) ->
+          let x0 = (x0f +. 1.) *. (float_of_int (width-1) ) /. 2.0  |> int_of_float in
+          let y0 = (y0f +. 1.) *. (float_of_int (height-1)) /. 2.0  |> int_of_float |> (fun y -> height-1-y) in
+          let x1 = (x1f +. 1.) *. (float_of_int (width-1) ) /. 2.0  |> int_of_float in
+          let y1 = (y1f +. 1.) *. (float_of_int (height-1)) /. 2.0  |> int_of_float |> (fun y -> height-1-y) in
+          Gopenl.line ~img ~color:green x0 y0 x1 y1
+        in
+        List.iter f [ p1,p2; p2,p3; p3,p1 ]
+      in
+      List.iter f data.triangles
+  end;
   Tga.write_tga ~filename:"out.tga" ~img
